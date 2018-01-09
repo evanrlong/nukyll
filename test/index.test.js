a0712327dc4996d8ecd515f3aa1d8972a85d0494
Evan Long <evanlong19@gmail.com>
2018-01-08 23:13:15 -0700

    Add Flow to module and clean up npm script formatting.

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,17 @@ import index from '~/index.js'
 describe('index', function () {
   describe('test()', function () {
     it('should return a string of "yay"', function () {
-      expect(index.test()).to.be.equal('test')
+      expect(index.test()).to.equal('test')
+    })
+  })
+
+  describe('test2()', function () {
+    it('should return a number of 2', function () {
+      expect(index.test2(2)).to.equal(2)
+    })
+
+    it('should return a number of 4', function () {
+      expect(index.test2(4)).to.equal(4)
     })
   })
 })
